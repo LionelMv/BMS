2b7cdbeee12b6023cafdd5210dcd04ab28e46b73
LionelMv <mvangicode@gmail.com>
2025-12-10 20:06:22 +0300

    feat: Add signal to create groups/roles
    Added a signal to create groups or roles after migrations

--- backend/apps/core/signals.py
+++ backend/apps/core/signals.py
@@ -1,9 +1,25 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
 from .models import User, Employee
+from django.contrib.auth.models import Group
 
 
 @receiver(post_save, sender=User)
-def create_employee_profile(sender, instance, created, **kwargs):
+def create_employee_profile(sender, instance: User, created: bool, **kwargs) -> None:
     if created:
         Employee.objects.create(user=instance)
+
+    else:
+        # ensure profile exists for existing user
+        Employee.objects.get_or_create(user=instance)
+
+@receiver(post_migrate)
+def create_default_groups(sender, **kwargs) -> None:
+    """
+    Create default groups/roles AFTER migrations.
+    """
+    if sender.name != "apps.core":
+        return
+
+    for name in ("Admin", "Manager", "Employee"):
+        Group.objects.get_or_create(name=name)
